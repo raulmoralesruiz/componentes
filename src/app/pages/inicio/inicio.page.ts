b7adfe3f22624049df815cbcab2b3844af8d6702
Raul Morales <raulmdaw@gmail.com>
2021-04-11 18:17:17 +0200

    Sección 5. Ion-avatar

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+interface ComponenteI {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+  
+  componentes: ComponenteI[] = [
+    {
+      icon: 'american-football-outline',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet',
+    },
+    {
+      icon: 'alert-circle-outline',
+      name: 'Alert',
+      redirectTo: '/alert',
+    },
+    {
+      icon: 'beaker-outline',
+      name: 'Avatar',
+      redirectTo: '/avatar',
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
