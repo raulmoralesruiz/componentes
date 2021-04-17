a7bf9f55ea23ead3bd10b30b45bbded907e8b41c
Raúl Morales <raulmdaw@gmail.com>
2021-04-17 14:32:48 +0200

    Sección 5. Ion-check

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -39,6 +39,11 @@ export class InicioPage implements OnInit {
       name: 'Cards',
       redirectTo: '/card',
     },
+    {
+      icon: 'checkmark-circle-outline',
+      name: 'Checks',
+      redirectTo: '/check',
+    },
   ];
 
   constructor() {}
